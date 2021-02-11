f07f6284a6128405ab5b1b13a031cbb15b9effe9
Forever-CodingNoob <johnnylin6256@gmail.com>
2021-02-11 17:45:42 +0800

    echo off

--- deploy.bat
+++ deploy.bat
@@ -1,11 +1,12 @@
+@echo off
 ECHO Running deploy commands...
 
 SET PROJECT_ID=brainfuck-linebot
 SET IMAGE_NAME=bot
-SET SERVICE_NAME=bot
+SET SERVICE_NAME=%IMAGE_NAME%
 
-gcloud builds submit --tag gcr.io/%PROJECT_ID%/%IMAGE_NAME%
+CMD /c gcloud builds submit --tag gcr.io/%PROJECT_ID%/%IMAGE_NAME%
 REM gcloud run deploy --image gcr.io/%PROJECT_ID%/%IMAGE_NAME% --platform managed
-gcloud run services update %SERVICE_NAME% --image gcr.io/%PROJECT_ID%/%IMAGE_NAME%:latest --platform=managed --region=asia-east1
+CMD /c gcloud run services update %SERVICE_NAME% --image gcr.io/%PROJECT_ID%/%IMAGE_NAME%:latest --platform=managed --region=asia-east1
 
 ECHO succeeded!
